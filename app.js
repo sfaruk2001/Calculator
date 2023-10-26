35059bc4507efb2ac3d8a337185805e71cd4a361
shihabfaruk <sfaruk3131p@gmail.com>
2023-10-25 23:31:20 -0400

    Finished the operate function.

--- app.js
+++ app.js
@@ -17,3 +17,19 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return num1 / num2;
 }
+
+function operate(num1, num2, operator) {
+    num1 = Number(num1);
+    num2 = Number(num2);
+    if (operator === '+') {
+        return add(num1, num2);
+    } else if (operator === '-') {
+        return subtract(num1, num2);
+    } else if (operator === 'x') {
+        return multiply(num1, num2);
+    } else if (operator === '÷') {
+        return divide(num1, num2);
+    } else {
+        return null;
+    }
+}
