bf6ea06b6c8b833b13ef4d81971b58d76c4acb53
shihabfaruk <sfaruk3131p@gmail.com>
2023-11-18 21:45:55 -0500

    Allowed display to change when a number key pressed.

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 let operand1 = "";
 let operand2 = "";
 let operator = "";
+let savedNumber = "";
 
-const numButtons = document.querySelectorAll('num');
-const operatorButtons = document.querySelectorAll('operator');
-const display = document.querySelector('display');
-const equals = document.querySelector('equals');
-const clear = document.querySelector('clear');
+const numButtons = document.querySelectorAll('.num');
+const operatorButtons = document.querySelectorAll('.operator');
+const display = document.querySelector('.displayContent');
+const equals = document.querySelector('.equals');
+const clear = document.querySelector('.clear');
 
+//add event listeners for number keys
+for (let i = 0; i < numButtons.length; i++) {
+    numButtons[i].addEventListener('click',displayScreen);
+}
+
+
+
+function displayScreen(e) {
+    let num = e.target.innerText;
+    display.innerHTML = num;
+    
+}
 
 function add(num1, num2) {
     return num1 + num2;
